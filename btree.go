314be8f2a8fe7d506779c36f4b4f7c8c5252aec0
Adit0507 <adi05072002@gmail.com>
2024-10-03 16:16:37 +0530

    settin up BTree

---
+++ btree.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+const HEADER = 4
+const BTREE_PAGE_SIZE = 4096
+const BTREE_MAX_KEY_SIZE = 1000
+const BTREE_MAX_VAL_SIZE = 3000
+
+func assert(cond bool) {
+	if !cond {
+		panic("assertion failure")
+	}
+}
+
+func init() {
+	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
+	assert(node1max <= BTREE_PAGE_SIZE)
+}
+
+// in memory data type
+type BNode []byte
+
+type BTree struct {
+	root uint64
+	get  func(uint64) []byte // dereferecne a pointer -- reads a page from disk
+	new  func([]byte) uint64 //alocates & writes a new page
+	del  func(uint64)        //delocate page
+}
+
+const (
+	BNODE_NODE = 1 //internal nodes without values
+	BNODE_LEAF = 2 //leaf nodes with values
+)
+
+func (node BNode) btype() uint16 {
+	return binary.LittleEndian.Uint16(node[0:2])
+}
+
+func (node BNode) nkeys() uint16 {
+	return binary.LittleEndian.Uint16(node[2:4])
+}
+
+func (node BNode) setHeader(btype uint16, nkeys uint16) {
+	binary.LittleEndian.PutUint16(node[0:2], btype)
+	binary.LittleEndian.PutUint16(node[2:4], nkeys)
+}
+
+// pointers
+func (node BNode) getPtr(idx uint16) uint64 {
+	assert(idx < node.nkeys())
+	pos := HEADER + 8*idx
+	return binary.LittleEndian.Uint64(node[pos:])
+}
+
+func (node BNode) setPtr(idx uint16, val uint64) {
+	assert(idx < node.nkeys())
+	pos := HEADER + 8*idx
+	binary.LittleEndian.PutUint64(node[pos:], val)
+}
+
+// offset list
+func offsetPos(node BNode, idx uint16) uint16 {
+	assert(1 <= idx && idx <= node.nkeys())
+
+	return HEADER + 8*node.nkeys() + 2*(idx-1)
+}
+
+func (node BNode) getOffset(idx uint16) uint16 {
+	if idx == 0 {
+		return 0
+	}
+
+	return binary.LittleEndian.Uint16(node[offsetPos(node, idx):])
+}
+
+func (node BNode) setOffset(idx uint16, offset uint16) {
+	binary.LittleEndian.PutUint16(node[offsetPos(node, idx):], offset)
+}
+
+func (node BNode) kvPos(idx uint16) uint16 {
+	assert(idx <= node.nkeys())
+
+	return HEADER + 8*node.nkeys() + 2*node.nkeys() + node.getOffset(idx)
+}
+
+func (node BNode) getKey(idx uint16) []byte {
+	assert(idx < node.nkeys())
+	pos := node.kvPos(idx)
+	klen := binary.LittleEndian.Uint16(node[pos:])
+
+	return node[pos+4:][:klen]
+}
+func (node BNode) getVal(idx uint16) []byte {
+	assert(idx < node.nkeys())
+	pos := node.kvPos(idx)
+	klen := binary.LittleEndian.Uint16(node[pos+0:])
+	vlen := binary.LittleEndian.Uint16(node[pos+2:])
+	return node[pos+4+klen:][:vlen]
+}
+
+func (node BNode) nbytes() uint16 {
+	return node.kvPos(node.nkeys())
+}
+
+func nodeLookupLE(node BNode, key []byte) uint16 {
+	nkeys := node.nkeys()
+	found := uint16(0)
+
+	for i := uint16(1); i < nkeys; i++ {
+		cmp := bytes.Compare(node.getKey(i), key)
+		if cmp <= 0 {
+			found = i
+		}
+		if cmp >= 0 {
+			break
+		}
+	}
+	return found
+}
+
