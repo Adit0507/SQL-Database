6d105d87e1429dd6d8faa3aad88f6a340a3f7fc8
Adit0507 <adi05072002@gmail.com>
2024-10-09 17:04:40 +0530

    internal tables to store schemes & metadata

--- table.go
+++ table.go
@@ -35,17 +35,41 @@ func (rec *Record) AddStr(col string, val []byte) *Record {
 }
 
 func (rec *Record) AddInt64(col string, val int64) *Record {
-    rec.Cols = append(rec.Cols, col)
+	rec.Cols = append(rec.Cols, col)
 	rec.Vals = append(rec.Vals, Value{Type: TYPE_INT64, I64: val})
 
 	return rec
 }
 func (rec *Record) Get(key string) *Value {
-	for i, c := range rec.Cols{
-		if c  == key{
+	for i, c := range rec.Cols {
+		if c == key {
 			return &rec.Vals[i]
 		}
 	}
 
 	return nil
+}
+
+// INTERNAL TABLES
+// store metadata
+var TDEF_META = &TableDef{
+	Prefix: 1,
+	Name:   "@meta",
+	Types:  []uint32{TYPE_BYTES, TYPE_BYTES},
+	Cols:   []string{"key", "val"},
+	PKeys:  1,
+}
+
+// store table schemas
+var TDEF_TABLE = &TableDef{
+	Prefix: 2,
+	Name:   "@table",
+	Types:  []uint32{TYPE_BYTES, TYPE_BYTES},
+	Cols:   []string{"name", "def"},
+	PKeys:  1,
+}
+
+var INTERNAL_TABLES map[string]*TableDef = map[string]*TableDef{
+	"@meta": TDEF_META,
+	"@table": TDEF_TABLE,
 }
