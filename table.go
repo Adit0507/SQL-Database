3eda4ece7b171fbf35383a1aaabac20e2be40d69
Adit0507 <adi05072002@gmail.com>
2024-10-09 16:46:10 +0530

    record methods

--- table.go
+++ table.go
@@ -1,17 +1,17 @@
 package main
 
 const (
-    TYPE_ERROR = 0
+	TYPE_ERROR = 0
 	TYPE_BYTES = 1
 	TYPE_INT64 = 2
 )
 
 type TableDef struct {
-    Name string
-    Types []uint32  //col type
-    Cols []string   //col name
-    PKeys int
-    Prefix uint32
+	Name   string
+	Types  []uint32 //col type
+	Cols   []string //col name
+	PKeys  int
+	Prefix uint32
 }
 
 // table cell
@@ -23,6 +23,29 @@ type Value struct {
 
 // represents a list of col names and values
 type Record struct {
-    Cols []string
-    Vals[]Value
+	Cols []string
+	Vals []Value
+}
+
+func (rec *Record) AddStr(col string, val []byte) *Record {
+	rec.Cols = append(rec.Cols, col)
+	rec.Vals = append(rec.Vals, Value{Type: TYPE_BYTES, Str: val})
+
+	return rec
+}
+
+func (rec *Record) AddInt64(col string, val int64) *Record {
+    rec.Cols = append(rec.Cols, col)
+	rec.Vals = append(rec.Vals, Value{Type: TYPE_INT64, I64: val})
+
+	return rec
+}
+func (rec *Record) Get(key string) *Value {
+	for i, c := range rec.Cols{
+		if c  == key{
+			return &rec.Vals[i]
+		}
+	}
+
+	return nil
 }
