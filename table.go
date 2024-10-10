fb0a479a08232e66af865f40e5a6158649baf23b
Adit0507 <adi05072002@gmail.com>
2024-10-10 10:18:42 +0530

    workin on Get

--- table.go
+++ table.go
@@ -6,6 +6,12 @@ const (
 	TYPE_INT64 = 2
 )
 
+type DB struct {
+	Path   string
+	kv     KV
+	tables map[string]*TableDef
+}
+
 type TableDef struct {
 	Name   string
 	Types  []uint32 //col type
@@ -70,6 +76,74 @@ var TDEF_TABLE = &TableDef{
 }
 
 var INTERNAL_TABLES map[string]*TableDef = map[string]*TableDef{
-	"@meta": TDEF_META,
+	"@meta":  TDEF_META,
 	"@table": TDEF_TABLE,
 }
+
+// reorder records to defined col. order
+func reorderRecord(tdef *TableDef, rec Record) ([]Value, error) {
+	assert(len(rec.Cols) == len(rec.Vals))
+	out := make([]Value, len(tdef.Cols))
+	for i, c :=  range tdef.Cols {
+		v := rec.Get(c)
+		if v == nil {
+			continue
+		}
+		if v.Type != tdef.Types[i] {
+			return nil, fmt.Errorf("bad column type: %s", c)
+		}
+		out[i] =*v
+	}
+
+	return out, nil
+}
+
+func valuesComplete(tdef *TableDef, vals []Value, n int) error {
+	for i, v:= range vals {
+		if i < n &&v.Type== 0 {
+			return fmt.Errorf("missing column: %s", tdef.Cols[i])
+		} else if i >= n && v.Type != 0{
+			return fmt.Errorf("extra column: %s", tdef.Cols[i])
+		}
+	}
+	
+	return nil
+}
+
+// check for missing columns
+func checkRecord(tdef *TableDef, rec Record, n int) ([]Value, error) {
+	vals, err := reorderRecord(tdef, rec)
+	if err != nil {
+		return nil, err
+	}
+
+	err = valuesComplete(tdef, vals, n)
+	if err!= nil {
+		return nil, err
+	}
+	return vals, nil
+}
+
+// get a single row by primary key
+func dbGet(db *DB, tdef *TableDef, rec *Record) (bool, error) {
+	vals, err := checkRecord(tdef, *rec, tdef.PKeys)
+
+	return false, nil
+}
+
+// get table schema by naem
+func getTableDef(db *DB, name string) *TableDef{
+	rec := (&Record{}).AddStr("name", []byte(name))	
+	ok, err := db
+
+}
+
+func (db *DB) Get(table string, rec *Record) (bool, error) {
+	tdef := 
+}
+
+
+func (db *DB) Insert(table string, rec Record) (bool, error)
+func (db *DB) Update(table string, rec Record) (bool, error)
+func (db *DB) Delete(table string, rec Record) (bool, error)
+func (db *DB) Upsert(table string, rec Record) (bool, error)
