ed791a37f58ca0ec15d9145a847ab79a86d72be6
Adit0507 <adi05072002@gmail.com>
2024-10-12 12:02:34 +0530

    updating child node & finding closest position to input key

--- btree_iter.go
+++ btree_iter.go
@@ -15,17 +15,33 @@ func (iter *BIter) Prev()
 
 func iterNext(iter *BIter, level int) {
 	if iter.pos[level]+1 < iter.path[level].nkeys() {
-		iter.pos[level]++
+		iter.pos[level]++ //move within node
 	} else if level > 0 {
-		iterNext(iter, level-1)
+		iterNext(iter, level-1) //move to sibling node
 	} else {
-		iter.pos[len(iter.pos)-1]++
+		iter.pos[len(iter.pos)-1]++ //past last key
 		return
+	}
+	if level+1 < len(iter.pos) { //update child node
+		node := iter.path[level]
+		kid := BNode(iter.tree.get(node.getPtr(iter.pos[level])))
+		iter.path[level+1] = kid
+		iter.pos[level+1] = 0
 	}
 }
 
 // find closest position that is less or equal to input key
-func (tree *BTree) SeekLE(key []byte) *BIter
+func (tree *BTree) SeekLE(key []byte) *BIter {
+	iter := &BIter{tree: tree}
+	for ptr := tree.root; ptr != 0; {
+		node := BNode(tree.get(ptr))
+		idx := nodeLookupLE(node, key)
+		iter.path = append(iter.path, node)
+		iter.pos = append(iter.pos, idx)
+		ptr = node.getPtr(idx)
+	}
+	return iter
+}
 
 // get current KV pair
 func (iter *BIter) Deref() ([]byte, []byte)
