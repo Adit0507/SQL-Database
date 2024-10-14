c6e4ff0279a090bc2b8aa00836d2cf20b970ff73
Adit0507 <adi05072002@gmail.com>
2024-10-14 14:00:27 +0530

    begin, abort & commit transactions

--- tx.go
+++ tx.go
@@ -1,23 +1,40 @@
 package main
 
+import "runtime"
+
 type KVTX struct {
 	db   *KV
-	meta []byte	//for rollback
+	meta []byte //for rollback
+	root uint64
+	done bool
 }
 
 // begin a transaction
 func (kv *KV) Begin(tx *KVTX) {
 	tx.db = kv
 	tx.meta = saveMeta(tx.db)
+	tx.root = tx.db.tree.root
+	assert(kv.page.nappend == 0 && len(kv.page.updates) == 0)
+	runtime.SetFinalizer(tx, func(x *KVTX) {
+		assert(tx.done)
+	})
 }
 
 // rollback on error
 func (kv *KV) Commit(tx *KVTX) error {
+	assert(!tx.done)
+	tx.done = true
+	if kv.tree.root == tx.root{
+		return nil
+	}
+
 	return updateOrRevert(tx.db, tx.meta)
 }
 
 // end transaction
 func (kv *KV) Abort(tx *KVTX) {
+	assert(!tx.done)
+	tx.done = true
 	loadMeta(tx.db, tx.meta)
 
 	// discard temporaries
