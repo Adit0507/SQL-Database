309ea21bc633bf6b53c9486abd23bba1530bc9e9
Adit0507 <adi05072002@gmail.com>
2024-10-16 12:17:19 +0530

    version no. in freelist

--- freelist.go
+++ freelist.go
@@ -16,14 +16,15 @@ func (node LNode) getNext() uint64 {
 func (node LNode) setNext(next uint64) {
 	binary.LittleEndian.PutUint64(node[0:8], next)
 }
-func (node LNode) getPtr(idx int) uint64 {
-	offset := FREE_LIST_HEADER + 8*idx
-	return binary.LittleEndian.Uint64(node[offset:])
+func (node LNode) getPtr(idx int) (uint64, uint64) {
+	offset := FREE_LIST_HEADER + 16*idx
+	return binary.LittleEndian.Uint64(node[offset:]), binary.LittleEndian.Uint64(node[offset+8:])
 }
-func (node LNode) setPtr(idx int, ptr uint64) {
+func (node LNode) setPtr(idx int, ptr uint64, version uint64) {
 	assert(idx < FREE_LIST_CAP)
-	offset := FREE_LIST_HEADER + 8*idx
-	binary.LittleEndian.PutUint64(node[offset:], ptr)
+	offset := FREE_LIST_HEADER + 16*idx
+	binary.LittleEndian.PutUint64(node[offset+0:], ptr)
+	binary.LittleEndian.PutUint64(node[offset+8:], version)
 }
 
 type FreeList struct {
@@ -43,10 +44,21 @@ type FreeList struct {
 
 	// in-memory states
 	maxSeq uint64 // saved tailSeq to prevnt consuming newly added items
+	maxVer uint64 //oldest reader version
+	curVer uint64 //version no. when commiting
 }
 
 func seq2idx(seq uint64) int {
 	return int(seq % FREE_LIST_CAP)
+}
+
+func versionBefore(a, b uint64) bool {
+	return a-b > 1<<63
+}
+
+func (fl *FreeList) SetMaxVer(maxVer uint64) {
+	fl.maxSeq = fl.tailSeq
+	fl.maxVer = maxVer
 }
 
 func (fl *FreeList) SetMaxSeq() {
@@ -55,46 +67,56 @@ func (fl *FreeList) SetMaxSeq() {
 
 // get 1 item form list head
 func (fl *FreeList) PopHead() uint64 {
-    ptr, head := flPop(fl)
-    if head != 0 {
-        fl.PushTail(head)
-    }
+	ptr, head := flPop(fl)
+	if head != 0 {
+		fl.PushTail(head)
+	}
 
-    return ptr
+	return ptr
+}
+
+func (fl *FreeList) check() {
+	assert(fl.headPage != 0 && fl.tailPage != 0)
+	assert(fl.headSeq != fl.tailSeq || fl.headPage == fl.tailPage)
 }
 
 func (fl *FreeList) PushTail(ptr uint64) {
-    // addin to tail node
-    LNode(fl.set(fl.tailPage)).setPtr(seq2idx(fl.tailSeq), ptr)
-    fl.tailSeq ++
+	fl.check()
+	// addin to tail node
+	LNode(fl.set(fl.tailPage)).setPtr(seq2idx(fl.tailSeq), ptr, fl.curVer)
+	fl.tailSeq++
 
-    if seq2idx(fl.tailSeq) == 0 {
-        next, head := flPop(fl)
-        if next == 0 {
-            // allocate new node by appending
-            next = fl.new(make([]byte, BTREE_PAGE_SIZE))
-        }
+	if seq2idx(fl.tailSeq) == 0 {
+		next, head := flPop(fl)
+		if next == 0 {
+			// allocate new node by appending
+			next = fl.new(make([]byte, BTREE_PAGE_SIZE))
+		}
 
-        // link to new tail node
-        LNode(fl.set(fl.tailPage)).setNext(next)
-        fl.tailPage = next
+		// link to new tail node
+		LNode(fl.set(fl.tailPage)).setNext(next)
+		fl.tailPage = next
 
-        // add head node if its removed
-        if head != 0 {
-            LNode(fl.set(fl.tailPage)).setPtr(0, head)
-            fl.tailSeq++       
-        }
-    }
+		// add head node if its removed
+		if head != 0 {
+			LNode(fl.set(fl.tailPage)).setPtr(0, head, fl.curVer)
+			fl.tailSeq++
+		}
+	}
 }
 
 // rmeove 1 item from head node & remove head node if empty
 func flPop(fl *FreeList) (ptr uint64, head uint64) {
+	fl.check()
 	if fl.headSeq == fl.maxSeq {
 		return 0, 0
 	}
 
 	node := LNode(fl.get(fl.headPage))
-	ptr = node.getPtr(seq2idx(fl.headSeq))
+	ptr, version := node.getPtr(seq2idx(fl.headSeq))
+	if versionBefore(fl.maxVer, version) {
+		return 0, 0
+	}
 	fl.headSeq++
 
 	if seq2idx(fl.headSeq) == 0 {
